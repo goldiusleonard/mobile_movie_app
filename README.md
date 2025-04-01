e4d7db8d1a21431d8c69c2565b41f2820635cb50
Goldius Leonard <goldius.leonard@gmail.com>
2025-04-02 01:42:12 +0800

    feat: intiial commit

---
+++ README.md
@@ -0,0 +1,75 @@
+# Welcome to your Expo app 👋
+
+This is an [Expo](https://expo.dev) project created with [`create-expo-app`](https://www.npmjs.com/package/create-expo-app).
+
+## Dependencies
+
+1. nativewind
+2. tailwindcss
+3. react-native-reanimated
+4. react-native-safe-area-context
+
+## Get started
+
+1. Install dependencies
+
+   ```bash
+   npm install nativewind tailwindcss react-native-reanimated react-native-safe-area-context
+   ```
+
+2. Initialize tailwindcss
+
+   ```bash
+   npx tailwindcss init
+   ```
+
+3. Initialize babel config js
+   
+   1) create `babel.config.js`.
+   2) copy from https://www.nativewind.dev/getting-started/installation.
+
+4. Initialize metro config js
+
+   1) ```bash
+      npx expo customize metro.config.js
+      ```
+   2) copy from https://www.nativewind.dev/getting-started/installation.
+
+2. Start the app
+
+   ```bash
+    npx expo start
+   ```
+
+In the output, you'll find options to open the app in a
+
+- [development build](https://docs.expo.dev/develop/development-builds/introduction/)
+- [Android emulator](https://docs.expo.dev/workflow/android-studio-emulator/)
+- [iOS simulator](https://docs.expo.dev/workflow/ios-simulator/)
+- [Expo Go](https://expo.dev/go), a limited sandbox for trying out app development with Expo
+
+You can start developing by editing the files inside the **app** directory. This project uses [file-based routing](https://docs.expo.dev/router/introduction).
+
+## Get a fresh project
+
+When you're ready, run:
+
+```bash
+npm run reset-project
+```
+
+This command will move the starter code to the **app-example** directory and create a blank **app** directory where you can start developing.
+
+## Learn more
+
+To learn more about developing your project with Expo, look at the following resources:
+
+- [Expo documentation](https://docs.expo.dev/): Learn fundamentals, or go into advanced topics with our [guides](https://docs.expo.dev/guides).
+- [Learn Expo tutorial](https://docs.expo.dev/tutorial/introduction/): Follow a step-by-step tutorial where you'll create a project that runs on Android, iOS, and the web.
+
+## Join the community
+
+Join our community of developers creating universal apps.
+
+- [Expo on GitHub](https://github.com/expo/expo): View our open source platform and contribute.
+- [Discord community](https://chat.expo.dev): Chat with Expo users and ask questions.
---
+++ app/(tabs)/search.tsx
@@ -0,0 +1,82 @@
+import { View, Text, Image, FlatList, ActivityIndicator } from "react-native";
+import React, { useState } from "react";
+import { images } from "@/constants/images";
+import MovieCard from "@/components/MovieCard";
+import useFetch from "@/services/useFetch";
+import { fetchMovies } from "@/services/api";
+import { icons } from "@/constants/icons";
+import { SearchBar } from "react-native-screens";
+
+const Search = () => {
+    const [searchQuery, setSearchQuery] = useState('');
+
+    const { 
+        data: movies,
+        loading,
+        error 
+    } = useFetch(() => fetchMovies({
+        query: searchQuery
+    }), false)
+
+    return (
+        <View className="flex-1 bg-primary">
+            <Image source={images.bg} className="flex-1 absolute w-full z-0" resizeMode="cover"/>
+            
+            <FlatList
+                data={movies}
+                renderItem={({ item }) => (
+                    <MovieCard
+                        {...item}
+                    />
+                )}
+                keyExtractor={(item) => item.id.toString()}
+                className="px-5"
+                numColumns={3}
+                columnWrapperStyle={{
+                    justifyContent: "center",
+                    gap: 16,
+                    marginVertical: 16
+                }}
+                contentContainerStyle={{
+                    paddingBottom: 100
+                }}
+                ListHeaderComponent={
+                    <>
+                        <View className="w-full flex-row justify-center mt-20 items-center">
+                            <Image source={icons.logo} className="w-12 h-10"></Image>
+                        </View>
+
+                        <View className="my-5">
+                            <SearchBar
+                                placeholder="Search movies ..."
+                            />
+                        </View>
+
+                        {loading && (
+                            <ActivityIndicator
+                                size="large"
+                                color="#0000ff"
+                                className="my-3"
+                            />
+                        )}
+
+                        {error && (
+                            <Text className="text-red-500 px-5 my-3">
+                                Error: {error?.message}
+                            </Text>
+                        )}
+
+                        {!loading && !error && searchQuery.trim() && movies?.length > 0 && (
+                            <Text className="text-xl text-white">
+                                Search Results for{' '}
+                                <Text className="text-accent">{searchQuery}</Text>
+                            </Text>
+                        )}
+                    </>
+                }
+            />
+        </View>
+    )
+}
+
+export default Search
